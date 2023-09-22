c0eca9bad6e8b4a301d287c0c6566cee61f7966a
shahrul <shahrul@homeez.com>
2023-09-22 23:12:26 +0800

    added eslint

--- src/app.tsx
+++ src/app.tsx
@@ -1,61 +1,70 @@
-// src/app.tsx
-import React from 'react';
-import crypto from 'crypto';
-import express, { Request as ExpressRequest, Response, NextFunction  } from 'express';
-const { renderToString } = require('react-dom/server');
+/* eslint-disable react/no-unknown-property */
+import React from 'react'
+import crypto from 'crypto'
+import express, { type Request as ExpressRequest, type Response, type NextFunction } from 'express'
+import { renderToString } from 'react-dom/server'
 
 interface Request extends ExpressRequest {
-    query: {
-      text: string;
-    };
+  query: {
+    text: string
+  }
 }
 
 declare module 'react' {
-    interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-      // extends React's HTMLAttributes
-      _?: string;
-      _1?: string;
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    // extends React's HTMLAttributes
+    _?: string
+  }
+}
+
+function processResponse (req: Request, res: Response, next: NextFunction): void {
+  const originalSend: (body?: any) => Response = res.send.bind(res)
+  res.send = function (data: any): Response {
+    if (typeof data === 'string') {
+      return originalSend(data)
     }
+    return originalSend(renderToString(data))
+  }
+  next()
 }
 
-function processResponse(req: Request, res: Response, next: NextFunction) {
-    const originalSend: (body?: any) => Response = res.send.bind(res); 
-    res.send = function (data: any): Response {
-        if (typeof data === 'string') {
-            return originalSend(data);
-        }
-        return originalSend(renderToString(data));
-    };
-    next();
-}
+const app = express()
 
-const app = express();
+app.use('/static', express.static('node_modules'))
 
-app.use('/static', express.static('node_modules'));
+app.use(processResponse)
 
-app.use(processResponse);
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000
 
 interface Todo {
-    id: string;
-    text: string;
-    completed: boolean;
-};
+  id: string
+  text: string
+  completed?: boolean
+  editing?: boolean
+}
 
 interface TodoProps {
-    id: string;
-    text: string;
-};
+  id: string
+  text: string
+  completed?: boolean
+  editing?: boolean
+}
 
 interface Todos {
-    todos: Todo[]; // An array of Todo objects
-};
+  todos: Todo[] // An array of Todo objects
+}
 
-const todos: Todo[] = []; // An empty array of Todo objects
+const todos: Todo[] = [] // An empty array of Todo objects
 
-const Todo: React.FC<TodoProps> = ({ id, text }) => (
-    <li key={id}>
+const activeClass = (completed: boolean | undefined, editing: boolean | undefined): string => {
+  let cl: string[] = []
+  if (completed ?? false) cl = cl.concat('completed')
+  if (editing ?? false) cl = cl.concat('editing')
+  return cl.join(' ')
+}
+
+const TodoItem: React.FC<TodoProps> = ({ id, text, completed, editing }) => (
+    <li key={id} className={activeClass(completed, editing)}>
         <div className="view">
             <input className="toggle" type="checkbox" />
             <label>{text}</label>
@@ -66,7 +75,7 @@ const Todo: React.FC<TodoProps> = ({ id, text }) => (
 )
 
 // const completed = todos.filter(c => c.completed)
-const uncompleted = () => todos.filter(c => !c.completed)
+const uncompleted = (): any => todos.filter(c => !(c.completed ?? false))
 
 const MainTemplate: React.FC<Todos> = ({ todos }) => (
     <html lang="en" data-framework="htmx">
@@ -79,12 +88,12 @@ const MainTemplate: React.FC<Todos> = ({ todos }) => (
         <body>
             <section className="todoapp" _="on load fetch /update-counts then put the result into .todo-count">
                 <header
-                    className="header" 
+                    className="header"
                     _="on keyup[keyCode==13] fetch /update-counts then put the result into .todo-count">
                     <h1>todos</h1>
-                    <input 
+                    <input
                         id="new-todo"
-                        name="text" 
+                        name="text"
                         className="new-todo"
                         placeholder="What needs to be done?"
                         hx-get="/new-todo"
@@ -97,7 +106,7 @@ const MainTemplate: React.FC<Todos> = ({ todos }) => (
                 <section className="main">
                     <input id="toggle-all" className="toggle-all" type="checkbox" />
                     <label htmlFor="toggle-all">Mark all as complete</label>
-                    <ul className="todo-list">{todos.map(Todo)}</ul>
+                    <ul className="todo-list">{todos.map(TodoItem)}</ul>
                 </section>
                 <footer className="footer">
                     <span className="todo-count" />
@@ -126,26 +135,25 @@ const MainTemplate: React.FC<Todos> = ({ todos }) => (
     </html>
 )
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(<MainTemplate todos={todos} />);
-});
+app.get('/', (req: Request, res: Response) => res.send(<MainTemplate todos={todos} />))
 
-app.get('/learn.json', (req: Request, res: Response) => res.send('{}'));
+app.get('/learn.json', (req: Request, res: Response) => res.send('{}'))
 
 app.get('/update-counts', (req: Request, res: Response) => res.send(
     <>
         <strong>{uncompleted().length}</strong>{` item${uncompleted().length === 1 ? '' : 's'} left`}
     </>
-));
+))
 
 app.get('/new-todo', (req: Request, res: Response) => {
-    // in a proper way this should always get sanitize
-    const { text } = req.query
-    const id = crypto.randomUUID()
-    todos.push({ id, text, completed: false })
-    res.send(
-        <Todo id={id} text={text} />
-    )
-});
+  // in a proper way this should always get sanitize
+  const { text } = req.query
+  const id = crypto.randomUUID()
+  const todo = { id, text, completed: false }
+  todos.push(todo)
+  res.send(
+        <TodoItem {...todo}/>
+  )
+})
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => { console.log(`Server is running on port ${port}`) })
