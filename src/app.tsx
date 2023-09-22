65d1f8a3125121df71a4c8c27173eb1c336ec01e
shahrul <shahrul@homeez.com>
2023-09-22 23:16:23 +0800

    added spacing rule

--- src/app.tsx
+++ src/app.tsx
@@ -64,75 +64,75 @@ const activeClass = (completed: boolean | undefined, editing: boolean | undefine
 }
 
 const TodoItem: React.FC<TodoProps> = ({ id, text, completed, editing }) => (
-    <li key={id} className={activeClass(completed, editing)}>
-        <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>{text}</label>
-            <button className="destroy" />
-        </div>
-        <input className="edit" />
-    </li>
+  <li key={id} className={activeClass(completed, editing)}>
+    <div className="view">
+      <input className="toggle" type="checkbox" />
+      <label>{text}</label>
+      <button className="destroy" />
+    </div>
+    <input className="edit" />
+  </li>
 )
 
 // const completed = todos.filter(c => c.completed)
 const uncompleted = (): any => todos.filter(c => !(c.completed ?? false))
 
 const MainTemplate: React.FC<Todos> = ({ todos }) => (
-    <html lang="en" data-framework="htmx">
-        <head>
-            <meta charSet="utf-8" />
-            <title>HTMX • TodoMVC</title>
-            <link rel="stylesheet" href="static/todomvc-common/base.css" type="text/css" />
-            <link rel="stylesheet" href="static/todomvc-app-css/index.css" type="text/css" />
-        </head>
-        <body>
-            <section className="todoapp" _="on load fetch /update-counts then put the result into .todo-count">
-                <header
-                    className="header"
-                    _="on keyup[keyCode==13] fetch /update-counts then put the result into .todo-count">
-                    <h1>todos</h1>
-                    <input
-                        id="new-todo"
-                        name="text"
-                        className="new-todo"
-                        placeholder="What needs to be done?"
-                        hx-get="/new-todo"
-                        hx-trigger="keyup[keyCode==13], text"
-                        hx-target=".todo-list"
-                        hx-swap="beforeend"
-                        _="on htmx:afterRequest set my value to ''"
-                        autoFocus />
-                </header>
-                <section className="main">
-                    <input id="toggle-all" className="toggle-all" type="checkbox" />
-                    <label htmlFor="toggle-all">Mark all as complete</label>
-                    <ul className="todo-list">{todos.map(TodoItem)}</ul>
-                </section>
-                <footer className="footer">
-                    <span className="todo-count" />
-                    <ul className="filters">
-                        <li>
-                            <a className="selected" href="#/">All</a>
-                        </li>
-                        <li>
-                            <a href="#/active">Active</a>
-                        </li>
-                        <li>
-                            <a href="#/completed">Completed</a>
-                        </li>
-                    </ul>
-                </footer>
-            </section>
-            <footer className="info">
-                <p>Double-click to edit a todo</p>
-                <p>Created by <a href="http://github.com/syarul/">syarul</a></p>
-                <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-            </footer>
-            <script src="static/todomvc-common/base.js" />
-            <script src="static/htmx.org/dist/htmx.js" />
-            <script src="static/hyperscript.org/dist/_hyperscript.min.js" />
-        </body>
-    </html>
+  <html lang="en" data-framework="htmx">
+    <head>
+      <meta charSet="utf-8" />
+      <title>HTMX • TodoMVC</title>
+      <link rel="stylesheet" href="static/todomvc-common/base.css" type="text/css" />
+      <link rel="stylesheet" href="static/todomvc-app-css/index.css" type="text/css" />
+    </head>
+    <body>
+      <section className="todoapp" _="on load fetch /update-counts then put the result into .todo-count">
+        <header
+          className="header"
+          _="on keyup[keyCode==13] fetch /update-counts then put the result into .todo-count">
+          <h1>todos</h1>
+          <input
+            id="new-todo"
+            name="text"
+            className="new-todo"
+            placeholder="What needs to be done?"
+            hx-get="/new-todo"
+            hx-trigger="keyup[keyCode==13], text"
+            hx-target=".todo-list"
+            hx-swap="beforeend"
+            _="on htmx:afterRequest set my value to ''"
+            autoFocus />
+        </header>
+        <section className="main">
+          <input id="toggle-all" className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">{todos.map(TodoItem)}</ul>
+        </section>
+        <footer className="footer">
+          <span className="todo-count" />
+          <ul className="filters">
+            <li>
+              <a className="selected" href="#/">All</a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+        </footer>
+      </section>
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>Created by <a href="http://github.com/syarul/">syarul</a></p>
+        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+      </footer>
+      <script src="static/todomvc-common/base.js" />
+      <script src="static/htmx.org/dist/htmx.js" />
+      <script src="static/hyperscript.org/dist/_hyperscript.min.js" />
+    </body>
+  </html>
 )
 
 app.get('/', (req: Request, res: Response) => res.send(<MainTemplate todos={todos} />))
@@ -140,9 +140,9 @@ app.get('/', (req: Request, res: Response) => res.send(<MainTemplate todos={todo
 app.get('/learn.json', (req: Request, res: Response) => res.send('{}'))
 
 app.get('/update-counts', (req: Request, res: Response) => res.send(
-    <>
-        <strong>{uncompleted().length}</strong>{` item${uncompleted().length === 1 ? '' : 's'} left`}
-    </>
+  <>
+    <strong>{uncompleted().length}</strong>{` item${uncompleted().length === 1 ? '' : 's'} left`}
+  </>
 ))
 
 app.get('/new-todo', (req: Request, res: Response) => {
@@ -152,7 +152,7 @@ app.get('/new-todo', (req: Request, res: Response) => {
   const todo = { id, text, completed: false }
   todos.push(todo)
   res.send(
-        <TodoItem {...todo}/>
+    <TodoItem {...todo}/>
   )
 })
 
